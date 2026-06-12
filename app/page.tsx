3065f6b4b18b4a0e6308602d9a4b04431abd7c03
turalgasimov <turallgasimov@gmail.com>
2026-06-12 14:07:05 -0400

    cleaned the branch

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import RecapExperience from "@/src/components/RecapExperience";
-import type { Recap } from "@/src/types/scene";
-import sample from "@/public/sample/sample-recap.json";
-
-export default function Home() {
-  return <RecapExperience recap={sample as unknown as Recap} />;
-}
